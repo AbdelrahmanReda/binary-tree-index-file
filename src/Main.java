c3d815cedf839cfe404705b5bec8b07b719878f0
Abdelrahman Reda <boodycat09@gmail.com>
2020-03-12 02:33:49 +0200

    add insertion function

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,13 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 
 public class Main {
-    static void readFile(String path){
+    static void readFile(String path) {
         try {
-            RandomAccessFile file = new RandomAccessFile(path,"rw");
-            for (int i = 0; i <file.length()/4 ; i++) {
-                System.out.println(file.readInt());
+            RandomAccessFile file = new RandomAccessFile(path, "rw");
+            for (int i = 0; i < file.length() / 4; i++) {
+                if (i%4==0 && i !=0)
+                    System.out.println();
+                System.out.print(" "+file.readInt());
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -15,21 +17,22 @@ public class Main {
             e.printStackTrace();
         }
     }
-    static void CreateRecordsFile (String filename, int numberOfRecords){
-        try {
-            RandomAccessFile treeFile = new RandomAccessFile(filename,"rw");
-            for (int i = 1; i <numberOfRecords ; i++) {
 
-                treeFile.writeInt(i);
-                treeFile.writeInt(0);
-                treeFile.writeInt(0);
-                treeFile.writeInt(0);
+    static void CreateRecordsFile(String filename, int numberOfRecords) {
+        try {
+            RandomAccessFile treeIndexFile = new RandomAccessFile(filename, "rw");
+            for (int i = 1; i < numberOfRecords; i++) {
+
+                treeIndexFile.writeInt(i);
+                treeIndexFile.writeInt(0);
+                treeIndexFile.writeInt(0);
+                treeIndexFile.writeInt(0);
 
             }
-            treeFile.writeInt(-1);
-            treeFile.writeInt(0);
-            treeFile.writeInt(0);
-            treeFile.writeInt(0);
+            treeIndexFile.writeInt(-1);
+            treeIndexFile.writeInt(0);
+            treeIndexFile.writeInt(0);
+            treeIndexFile.writeInt(0);
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -37,14 +40,132 @@ public class Main {
             e.printStackTrace();
         }
     }
-    int InsertNewRecordAtIndex (String filename, int Key, int ByteOffset){return 0; }
-    void SearchRecordInIndex (String filename, int Key){}
-    void DisplayBinarySearchTreeInOrder (String FileName){}
-    void DisplayIndexFileContent (String filename){}
+    static void handleReferences(RandomAccessFile file, int offset, int reference){
+        try {
+            file.seek(0);
+            file.seek(16);
+            int x = file.readInt();
+            while (true)
+            {
+                if (x<offset)
+                {
+                    file.readInt();
+                    file.readInt();
+                    int leftLocation = (int)file.getFilePointer();
+                    int left = file.readInt();
+                    if (left==-1)
+                    {
+                        file.seek(leftLocation);
+                        file.writeInt(reference);
+                        break;
+                    }
+                    else
+                    {
+                        System.out.println("leftLocation::"+left);
 
-    public static void main (String args[]){
-        String treeFile = "src/containers/tree,bin";
-        CreateRecordsFile(treeFile,8);
-        readFile(treeFile);
+                        file.seek(left*16);
+                        x=file.readInt();
+                    }
+                }
+                else
+                {
+                    file.readInt();
+                    int leftLocation = (int)file.getFilePointer();
+                    int left = file.readInt();
+                    if (left==-1)
+                    {
+                        file.seek(leftLocation);
+                        file.writeInt(reference);
+                        break;
+                    }
+                    else
+                    {
+                        file.seek(left*16);
+                        x=file.readInt();
+                    }
+
+                }
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static int InsertNewRecordAtIndex(String filename, int Key, int ByteOffset) {
+        //open the tree file
+        try {
+            RandomAccessFile treeIndexFile = new RandomAccessFile(filename, "rw");
+            int freeRecordIndex = treeIndexFile.readInt();
+            treeIndexFile.seek(0);
+            if ((freeRecordIndex+1)==treeIndexFile.length()/16)
+            {
+                treeIndexFile.writeInt(-1);
+            }
+            else
+            {
+                treeIndexFile.writeInt(freeRecordIndex+1);
+            }
+            if (freeRecordIndex != -1) {
+                if (freeRecordIndex==1)
+                {
+                    System.out.println("insertin  first record");
+                    treeIndexFile.seek(0);
+                    treeIndexFile.seek(16);
+                    treeIndexFile.writeInt(Key);
+                    treeIndexFile.writeInt(ByteOffset);
+                    treeIndexFile.writeInt(-1);
+                    treeIndexFile.writeInt(-1);
+                }
+                else
+                {
+                    System.out.println("insertin non first record");
+                    /*seek to the proper place to insert any new record*/
+                    treeIndexFile.seek(freeRecordIndex*16);
+                    treeIndexFile.writeInt(Key);
+                    treeIndexFile.writeInt(ByteOffset);
+                    treeIndexFile.writeInt(-1);
+                    treeIndexFile.writeInt(-1);
+                    handleReferences(treeIndexFile,Key,freeRecordIndex);
+                }
+
+            }else{
+                System.out.println("can't insert to file");
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return 0;
+    }
+    void SearchRecordInIndex(String filename, int Key) {
+    }
+
+    void DisplayBinarySearchTreeInOrder(String FileName) {
+    }
+
+    void DisplayIndexFileContent(String filename) {
+    }
+
+    public static void main(String args[]) {
+        String treeIndexFile = "src/containers/tree,bin";
+        CreateRecordsFile(treeIndexFile, 8);
+      //  readFile(treeIndexFile);
+        InsertNewRecordAtIndex(treeIndexFile,5,12);
+        InsertNewRecordAtIndex(treeIndexFile,12,24);
+        InsertNewRecordAtIndex(treeIndexFile,3,36);
+        InsertNewRecordAtIndex(treeIndexFile,9,48);
+        InsertNewRecordAtIndex(treeIndexFile,8,60);
+        InsertNewRecordAtIndex(treeIndexFile,2,72);
+        InsertNewRecordAtIndex(treeIndexFile,4,84);
+
+
+
+
+        readFile(treeIndexFile);
+
     }
 }
